83cf0b584218ac5a46f765d0d5b16b296e546dcb
PNZBEIJINGL <63351525@qq.com>
2019-06-09 10:43:27 +0800

    basic

--- src/main/java/com/groovytest/helloworld/Helloworld.groovy
+++ src/main/java/com/groovytest/helloworld/Helloworld.groovy
@@ -2,9 +2,11 @@ package com.groovytest.helloworld
 
 class Helloworld {
 
-    //不包含public修饰符 ， 参数也没有类型
+    //1.不包含public修饰符 ， 参数也没有类型
     static void main(args) {
         print("Hello world")
     }
+
+    //2.语法简单甚至可以删除只留下print("Hello world")
 
 }
---
+++ src/main/java/com/groovytest/helloworld/Helloworld2.groovy
@@ -0,0 +1,5 @@
+package com.groovytest.helloworld
+
+
+print("Hello world");
+
